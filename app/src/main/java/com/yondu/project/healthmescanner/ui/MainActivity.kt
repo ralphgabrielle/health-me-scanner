31b64a4741bb96a6748665a5def11b7f8b2d6c9c
Ralph Gabrielle <rorden@yondu.com>
2020-06-10 17:08:11 +0800

    Fix qr double scanned

--- app/src/main/java/com/yondu/project/healthmescanner/ui/MainActivity.kt
+++ app/src/main/java/com/yondu/project/healthmescanner/ui/MainActivity.kt
@@ -7,16 +7,13 @@ import com.yondu.project.healthmescanner.base.BaseActivity
 import com.yondu.project.healthmescanner.ui.log.LogActivity
 import com.yondu.project.healthmescanner.util.PreferenceManager
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.*
-import org.jetbrains.anko.clearTask
-import org.jetbrains.anko.intentFor
-import org.jetbrains.anko.newTask
 import org.jetbrains.anko.startActivity
 import org.koin.android.ext.android.inject
 
 class MainActivity : BaseActivity(), QRCodeReaderView.OnQRCodeReadListener {
 
     private val preferenceManager: PreferenceManager by inject()
+    private var hasScanned = false
 
     override val layoutId: Int?
         get() = R.layout.activity_main
@@ -28,23 +25,25 @@ class MainActivity : BaseActivity(), QRCodeReaderView.OnQRCodeReadListener {
         get() = R.string.header_qr
 
     override fun viewCreated() {
+        qrCode.setAutofocusInterval(2000L)
         qrCode.setOnQRCodeReadListener(this)
 
-        CoroutineScope(Dispatchers.IO).launch {
-
-            // delay 3 seconds
-            delay(3000)
-
-            withContext(Dispatchers.Main) {
-                startActivity<LogActivity>(
-                    "QR" to "5ed3a25a0f7a6f702b29bf33"
-                )
-            }
-        }
+//        CoroutineScope(Dispatchers.IO).launch {
+//
+//            // delay 3 seconds
+//            delay(3000)
+//
+//            withContext(Dispatchers.Main) {
+//                startActivity<LogActivity>(
+//                    "QR" to "5ed3a25a0f7a6f702b29bf33"
+//                )
+//            }
+//        }
     }
 
     override fun onResume() {
         super.onResume()
+        hasScanned = false
         qrCode.startCamera()
     }
 
@@ -60,6 +59,9 @@ class MainActivity : BaseActivity(), QRCodeReaderView.OnQRCodeReadListener {
 
     override fun onQRCodeRead(text: String?, points: Array<out PointF>?) {
         qrCode.stopCamera()
+
+        if (hasScanned) return
+        hasScanned = true
 
         startActivity<LogActivity>(
             "QR" to text
